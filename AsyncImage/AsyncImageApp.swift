0a8417c6cfb6226d93b3a0a0f3b5d642aa440d59
akiranishimura <akisayu0429@gmail.com>
2022-03-03 22:50:51 +0900

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by akira nishimura on 2022/03/03.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
